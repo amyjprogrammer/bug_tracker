d1ee6f9f1d6840abfea95d33d279d45f368512dc
Amy Jones <amyjprogrammer@gmail.com>
2021-02-17 03:20:30 -0500

    Models

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+"""Entering the bug info and having the Admin set status and priority"""
 
-# Create your models here.
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+
+class Ticket(models.Model):
+    ticket_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    ticket_choices = [
+        ('Bug', 'Bug'),
+        ('Enhancement', 'Enhancement'),
+        ('New_Feature', 'New_Feature'),
+    ]
+    ticket_choice = models.CharField(max_length=20, choices=ticket_choices, default='Bug')
+    issue_description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.ticket_author
+
+class TicketComment(models.Model):
+    comment = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    comment_author = models.CharField(max_length=75)
+    comment_text = models.CharField(max_length=350)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class AdminTicket(models.Model):
+    #allows the admin to assign a priority and status to the ticket
+    admin_ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    priority_choices =[
+        ("Critical", 'Critical'),
+        ('High', 'High'),
+        ('Normal', 'Normal'),
+    ]
+    priority_choice = models.CharField(max_length=10, choices=priority_choices, default= "Normal")
+    status_choices =[
+        ('Open', 'Open'),
+        ('Pending', 'Pending'),
+        ('Closed', 'Closed'),
+    ]
+    status_choice = models.CharField(max_length=10, choices=status_choices, default="Open")
+    additional_comments = models.CharField(max_length=350)
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-#Home page
+#Home page will show all the open tickets (no login required)
 def home(request):
     return render(request, 'issue_tracker/home.html')
