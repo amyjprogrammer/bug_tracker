e62cb84f3c51f40e49d5144a0b0c0f208a3e971e
Amy Jones <amyjprogrammer@gmail.com>
2021-02-18 01:41:11 -0500

    fixing the home page and navbar

--- bug_tracker/settings.py
+++ bug_tracker/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #my apps
     'issue_tracker',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 from dotenv import load_dotenv
 load_dotenv()
