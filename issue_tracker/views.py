2c2e60b05bbf4c31a767c6afa73f5501cc98044e
Amy Jones <amyjprogrammer@gmail.com>
2021-02-19 19:02:23 -0500

    added Pagination to home page

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 from .models import Ticket, TicketComment, AdminTicket
 
 #Home page will show all the open tickets (no login required)
 def home(request):
-    tickets = Ticket.objects.all()
-    context = {'tickets': tickets}
+    tickets = Ticket.objects.all().order_by('-date_added')
+    paginator = Paginator(tickets,10) # Show 2 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'issue_tracker/home.html', context)
 
 def ticket_comment(request, ticket_id):
