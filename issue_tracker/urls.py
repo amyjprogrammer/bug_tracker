e5611ed12f566179b63d92a4e3dd37e34b8a87f8
Amy Jones <amyjprogrammer@gmail.com>
2021-02-19 21:18:10 -0500

    new Ticket Comment page, view and pagination

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -7,4 +7,5 @@ app_name = 'issue_tracker'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:ticket_id>/', views.ticket_comment, name="ticket_comment"),
+    path('comments/<int:ticket_id>/', views.add_ticket_comment, name="add_ticket_comment"),
 ]
--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -10,7 +10,7 @@ from .models import Ticket, TicketComment, AdminTicket
 #Home page will show all the open tickets (no login required)
 def home(request):
     tickets = Ticket.objects.all().order_by('-date_added')
-    paginator = Paginator(tickets,10) # Show 2 contacts per page.
+    paginator = Paginator(tickets,10) # Show 10 contacts per page.
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -19,5 +19,15 @@ def home(request):
 
 def ticket_comment(request, ticket_id):
     tickets = Ticket.objects.filter(id=ticket_id)
-    context = {'tickets': tickets}
+    ticket = get_object_or_404(Ticket, id=ticket_id)
+    ticket_comments = ticket.comments.all().order_by('-created_date')
+    paginator = Paginator(ticket_comments, 10) # Show 10 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'tickets': tickets, 'page_obj' : page_obj, 'ticket':ticket}
     return render(request, 'issue_tracker/ticket_comment.html', context)
+
+
+def add_ticket_comment(request, ticket_id):
+    return render(request, 'issue_tracker/add_ticket_comment.html')
