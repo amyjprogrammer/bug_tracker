571a39f8fa57d43631758f8af004a9de581b00d6
Amy Jones <amyjprogrammer@gmail.com>
2021-02-23 19:09:16 -0500

    Customizing the Admin page

--- issue_tracker/admin.py
+++ issue_tracker/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Ticket, TicketComment, AdminTicket
 
-admin.site.register(Ticket)
-admin.site.register(TicketComment)
-admin.site.register(AdminTicket)
+admin.site.site_header= "Bug Tracker"
+admin.site.title= 'Admin Bug Tracker'
+
+class InlineTicketComment(admin.StackedInline):
+    model = TicketComment
+    extra = 3
+
+class InlineAdminTicket(admin.StackedInline):
+    model = AdminTicket
+    extra = 1
+
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [ InlineAdminTicket, InlineTicketComment,]
+    list_display = ('title', 'issue_description','ticket_choice')
+    list_filter = ('ticket_choice',)
+    search_fields = ('title','issue_description')
+
+admin.site.register(Ticket, TicketAdmin)
